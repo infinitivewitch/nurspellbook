8f22a690828e4e857c5219ebb375464484dcfeab
Mecha Sorceress <infinitivewitch+bot@disroot.org>
2023-04-16 20:08:26 +0000

    ci: update nvfetcher sources
    Signed-off-by: Mecha Sorceress <infinitivewitch+bot@disroot.org>

--- scrolls/packages/pkgs/_sources/generated.nix
+++ scrolls/packages/pkgs/_sources/generated.nix
@@ -17,4 +17,16 @@
     };
     date = "2023-04-15";
   };
+  nixpkgs-master = {
+    pname = "nixpkgs-master";
+    version = "6bf4cde63f78b2d58346d2574beb6c49c23ff247";
+    src = fetchFromGitHub {
+      owner = "NixOS";
+      repo = "nixpkgs";
+      rev = "6bf4cde63f78b2d58346d2574beb6c49c23ff247";
+      fetchSubmodules = false;
+      sha256 = "sha256-0m+KR2/TPU95LoGoyEdbEkjDlbpQ6af0y+SkGcMaxIY=";
+    };
+    date = "2023-04-16";
+  };
 }
